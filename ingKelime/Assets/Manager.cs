2c907f7451ead39c8dbbf277e0b00e1650893db1
yaemtaUni <yaemta@gmail.com>
2020-09-18 15:16:17 +0300

    ekranlar eklendi

---
+++ ingKelime/Assets/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class Manager : MonoBehaviour
+{
+    List<kelimeClass> kelimeListesi = new List<kelimeClass>();
+    string path = "Assets/Resources/test.txt";
+    void Start()
+    {
+
+        // listeyi array e çevirme
+        kelimeClass[] kelimeArray = kelimeListesi.ToArray();
+
+        // arraye çevirilmiş listeyi jsona çevirme
+        string json = JsonHelper.ToJson(kelimeArray);
+
+        // Dosyaya yazmak için
+        
+        StreamWriter writer = new StreamWriter(path, false);
+        writer.Write (json);
+        writer.Close();
+
+        // dosyadan okumak için
+        StreamReader reader = new StreamReader(path);
+        string str1 = reader.ReadToEnd().ToString();
+        reader.Close();
+
+        // json dan class array e çevirme
+        kelimeClass [] dejsonArroy = JsonHelper.FromJson<kelimeClass> (json);
+
+    }
+
+
+    
+}
---
+++ ingKelime/Assets/kelimeClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class kelimeClass
+{
+    public string ing;
+    public string tr;
+    public int sonDogruSayisi;
+
+
+}
